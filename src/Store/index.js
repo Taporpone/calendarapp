9f768bdbc3b5a6b9ac44dd4291ad6990084770ad
Konrad Szwarc <taporpone@gmail.com>
2017-05-05 19:28:04 +0200

    Store init
    Loaded with middleware

---
+++ src/Store/index.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux';
+import {createLogger} from 'redux-logger';
+import promise from 'redux-promise-middleware';
+import thunk from 'redux-thunk';
+
+import reducers from '../Reducers';
+
+const middleware = applyMiddleware(promise(), thunk, createLogger());
+
+// window.__REDUX -> ReduxDevTools extension for Chrome
+createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),middleware);
+
+export default store;
