429c9d4f27650aefbe21f12507445f9818ad05a7
Konrad Szwarc <taporpone@gmail.com>
2017-05-05 20:02:16 +0200

    Global view implemented

--- src/Containers/CalendarApp.js
+++ src/Containers/CalendarApp.js
@@ -1,30 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import {fetchUsers, fetchUser, postWeek } from '../Actions/calendarApp_actions';
+import { fetchUsers, fetchUser, postWeek } from '../Actions/calendarApp_actions';
+
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
 class CalendarApp extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
     }
-    componentWillMount(){
+    componentWillMount() {
         this.props.dispatch(fetchUsers());
     }
-    render(){
-        return(
-        <div>
-            <select>
-                {this.props.users.map(user => {
-                    return (
-                        <option key={user.id} value={user.id}>{user.username}</option>
-                    )
-                })}
-            </select>
-        </div>
-        )}
+    render() {
+        return (
+            <div>
+                <div>
+                    <select>
+                        {this.props.users.map(user => {
+                            return (
+                                <option key={user.id} value={user.id}>{user.username}</option>
+                            )
+                        })}
+                    </select>
+                </div>
+                <div>
+                    <DayPicker />
+                </div>
+                <div>
+                    <button>Accept</button>
+                    <button>Reject</button>
+                </div>
+            </div>
+        )
+    }
 };
 
-const mapStateToProps = function(store){
+const mapStateToProps = function (store) {
     return {
         users: store.calendarAppReducer.users,
         selectedMonth: store.calendarAppReducer.selectedUser
