338099c9cdb19cca9661bcf835ead5ec7c243687
Konrad Szwarc <taporpone@gmail.com>
2017-05-05 19:27:33 +0200

    Combine reducers
    Init

---
+++ src/Reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import calendarAppReducer from './calendar_reducer.js';
+
+const reducers = combineReducers({
+    calendarAppReducer
+});
+
+export default reducers;
