6629f4b11f9a91a9320ef46784e44ed584ca8a98
Konrad Szwarc <taporpone@gmail.com>
2017-05-05 19:10:35 +0200

    API actions init
    Added basic API actions

---
+++ src/Actions/calendarApp_actions.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+export const FETCH_USERS = 'FETCH_USERS';
+export const FETCH_USERS_FULFILLED = 'FETCH_USERS_FULFILLED';
+export const FETCH_USERS_REJECTED = 'FETCH_USERS_REJECTED';
+
+export const FETCH_USER = 'FETCH_USER';
+export const FETCH_USER_FULFILLED = 'FETCH_USER_FULFILLED';
+export const FETCH_USER_REJECTED = 'FETCH_USER_REJECTED';
+
+export const POST_WEEK = 'POST_WEEK';
+export const POST_WEEK_REJECTED = 'POST_WEEK_REJECTED';
+
+export function fetchUsers() {
+    return function (dispatch) {
+        axios.get('https://timesheet-staging-aurity.herokuapp.com/api/users')
+            .then((res) => {
+                dispatch({ type: FETCH_USERS_FULFILLED, payload: res.data });
+            })
+            .catch((err) => {
+                dispatch({ type: FETCH_USERS_REJECTED, payload: err })
+            })
+    }
+}
+
+export function fetchUser(month, user) {
+    return function (dispatch) {
+        axios.get(`https://timesheet-staging-aurity.herokuapp.com/api/training/weeks/${month}/2017/${user}`)
+            .then((res) => {
+                dispatch({ type: FETCH_USER_FULFILLED, payload: res.data.data})
+            })
+            .catch((err) => {
+                dispatch({ type: FETCH_USER_REJECTED, payload: err})
+            })
+    }
+}
+
+export function postWeek(week,approved, week_status){
+    return function(dispatch) {
+        axios.post(`https://timesheet-staging-aurity.herokuapp.com/api/training/weeks/${week}/users/${approved}`, {status: week_status})
+            .catch((err) => {
+                dispatch({ type: POST_WEEK_REJECTED, payload: err});
+            })
+    }
+}
