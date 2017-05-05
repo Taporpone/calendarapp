62c5fb7fd2e8eebbce63c29f65b6676079051734
Konrad Szwarc <taporpone@gmail.com>
2017-05-05 19:50:54 +0200

    Won battle with void

--- src/Store/index.js
+++ src/Store/index.js
@@ -8,6 +8,6 @@ import reducers from '../Reducers';
 const middleware = applyMiddleware(promise(), thunk, createLogger());
 
 // window.__REDUX -> ReduxDevTools extension for Chrome
-createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),middleware);
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),middleware);
 
 export default store;
