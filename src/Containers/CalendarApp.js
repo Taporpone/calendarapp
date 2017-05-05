73528ca4ba8eeaa0a14a7c580e177c3e6a4e0c61
Konrad Szwarc <taporpone@gmail.com>
2017-05-05 20:31:13 +0200

    User and month change triggers API call
    It does!

--- src/Containers/CalendarApp.js
+++ src/Containers/CalendarApp.js
@@ -13,13 +13,21 @@ class CalendarApp extends Component {
     componentWillMount() {
         const date = new Date();
         this.props.dispatch(fetchUsers());
-        this.props.dispatch(fetchUser(date.getMonth() + 1, 1))
+        this.props.dispatch(fetchUser(date.getMonth() + 1, 1));
+        this.props.dispatch(currentMonth(date.getMonth() + 1));
+    }
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.currentMonth !== this.props.currentMonth) {
+            this.props.dispatch(fetchUser(nextProps.currentMonth, this.props.currentUser))
+        } else if (nextProps.currentUser !== this.props.currentUser) {
+            this.props.dispatch(fetchUser(this.props.currentMonth, nextProps.currentUser))
+        }
     }
     render() {
         return (
             <div>
                 <div>
-                    <select onChange={ event => this.props.dispatch(currentUser(event.target.value))}>
+                    <select onChange={event => this.props.dispatch(currentUser(event.target.value))}>
                         {this.props.users.map(user => {
                             return (
                                 <option key={user.id} value={user.id}>{user.username}</option>
@@ -28,7 +36,9 @@ class CalendarApp extends Component {
                     </select>
                 </div>
                 <div>
-                    <DayPicker />
+                    <DayPicker
+                        onMonthChange={month => this.props.dispatch(currentMonth(month.getMonth() + 1))}
+                    />
                 </div>
                 <div>
                     <button>Accept</button>
@@ -42,7 +52,9 @@ class CalendarApp extends Component {
 const mapStateToProps = function (store) {
     return {
         users: store.calendarAppReducer.users,
-        selectedUser: store.calendarAppReducer.selectedUser
+        selectedUser: store.calendarAppReducer.selectedUser,
+        currentMonth: store.calendarAppReducer.currentMonth,
+        currentUser: store.calendarAppReducer.currentUser
     };
 };
 
