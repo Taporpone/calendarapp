dafcd4757c339f61ee805183bd4237a596697ffc
Konrad Szwarc <taporpone@gmail.com>
2017-05-06 16:59:44 +0200

    Implemented highliting choosen week

--- src/Containers/CalendarApp.js
+++ src/Containers/CalendarApp.js
@@ -10,6 +10,11 @@ import 'react-day-picker/lib/style.css';
 class CalendarApp extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            from: null,
+            to: null
+        };
+        this.handleDayClick = this.handleDayClick.bind(this);
     }
     componentWillMount() {
         const date = new Date();
@@ -24,7 +29,23 @@ class CalendarApp extends Component {
             this.props.dispatch(fetchUser(this.props.currentMonth, nextProps.currentUser))
         }
     }
+    handleDayClick(day) {
+        //Clicking day get to both return week and select whole week
+        const momentDay = moment(day);
+        let range = {
+            from: momentDay.day('Monday').toDate(),
+            to: momentDay.day('Sunday').add(1, 'week').toDate()
+        };
+        if (moment(day).day() === 0) {
+            range = {
+                from: moment(day).toDate(),
+                to: moment(day).subtract(6, 'days').toDate()
+            }
+        }
+        this.setState(range);
+    }
     render() {
+        const { from, to } = this.state;
         return (
             <div>
                 <div>
@@ -39,18 +60,20 @@ class CalendarApp extends Component {
                 <div>
                     <DayPicker
                         onMonthChange={month => this.props.dispatch(currentMonth(month.getMonth() + 1))}
+                        selectedDays={[from, { from, to }]}
+                        onDayClick={day => this.handleDayClick(day)}
                         renderDay={day => {
-                            if(!this.props.selectedUser || !this.props.selectedUser.days){
+                            if (!this.props.selectedUser || !this.props.selectedUser.days) {
                                 return <div><span>{day.getDay()}</span></div>
                             }
                             const dayOfMonth = moment(day).date();
                             const { days } = this.props.selectedUser;
                             const dayDetails = days[dayOfMonth];
                             let formattedTime = '0:0';
-                            if (dayDetails && dayDetails.hours && dayDetails.minutes){
+                            if (dayDetails && dayDetails.hours && dayDetails.minutes) {
                                 formattedTime = `${dayDetails.hour}:${dayDetails.minutes}`;
                             }
-                            return <div><span>{dayOfMonth}</span><br/><span>{formattedTime}</span></div>
+                            return <div><span>{dayOfMonth}</span><br /><span>{formattedTime}</span></div>
                         }}
                     />
                 </div>
